17b3687e7ce3fe7c71859fe312474ec75059f0a8
raziel0110 <albangabriel42@gmail.com>
2024-02-04 01:01:05 +0200

    Refactor profile page

--- resources/views/token/partials/update-token-form.blade.php
+++ resources/views/token/partials/update-token-form.blade.php
@@ -3,17 +3,36 @@
         {{ __('Token For Mobile Application') }}
     </h2>
 
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
-        @csrf
-        @method('patch')
+    @if ($user->token)
+        <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+            @csrf
+            @method('patch')
 
-        <div>
-            <x-input-label for="Token" :value="__('Token')" />
-            <x-text-input id="token" name="Token" type="text" class="mt-1 block w-full" :value="old('token', $user->token)" required autofocus autocomplete="token" />
-            <x-input-error class="mt-2" :messages="$errors->get('token')" />
-        </div>
+            <div>
+                <x-input-label for="Token" :value="__('Token')" />
+                <x-text-input id="token" name="Token" type="text" disabled class="mt-1 block w-full" :value="old('token', $user->token)" required autofocus autocomplete="token" />
+            </div>
 
-        <div class="flex items-center gap-4">
+            <div class="flex items-center gap-4">
+                <x-primary-button>{{__('Generate Token')}}</x-primary-button>
+                @if (session('status') === 'token-updated')
+                    <p
+                        x-data="{show: true}"
+                        x-show="show"
+                        x-transition
+                        x-init="setTimeout(() => show = false, 2000)"
+                        class="text-sm text-gray-600 dark:text-gray-400"
+                    >
+                    {{__('Token Generated')}}
+                    </p>
+                @endif
+            </div>
+        </form>
+    @else
+        <p class="text-sm text-gray-600 dark:text-gray-400">
+            {{ __("The token needs to be generated!") }}
+        </p>
+        <div class="flex items-center gap-4 mt-3">
             <x-primary-button>{{__('Generate Token')}}</x-primary-button>
             @if (session('status') === 'token-updated')
                 <p
@@ -27,5 +46,5 @@
                 </p>
             @endif
         </div>
-    </form>
+    @endif
 </section>
